82274daeee1fc4b224c5cf1ac54daef659a00325
Bryan H <thehabes@github.io>
2019-07-31 11:39:11 -0500

    test

--- js/leafletHelper.js
+++ js/leafletHelper.js
@@ -78,7 +78,7 @@
                 features = JSON.parse(JSON.stringify(geoAnno))
             }            
             
-            leafy.mymap = L.map('leafletInstanceContainer').setView(coords, 14)    
+            leafy.mymap = L.map('leafletInstanceContainer').setView(coords, 18)    
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGhlaGFiZXMiLCJhIjoiY2pyaTdmNGUzMzQwdDQzcGRwd21ieHF3NCJ9.SSflgKbI8tLQOo2DuzEgRQ', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 100,
