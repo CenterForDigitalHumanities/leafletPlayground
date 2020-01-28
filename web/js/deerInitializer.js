90a04f0cc1c173120e40f48df1a0909cf570f693
Bryan H <thehabes@github.io>
2020-01-28 09:02:25 -0600

    Bring in a DEER version of this

---
+++ web/js/deerInitializer.js
@@ -0,0 +1,28 @@
+/**
+ * @module DEER Data Encoding and Exhibition for RERUM (DEER)
+ * @author Patrick Cuba <cubap@slu.edu>
+
+ * This code should serve as a basis for developers wishing to
+ * use TinyThings as a RERUM proxy for an application for data entry,
+ * especially within the Eventities model.
+ * @see tiny.rerum.io
+ */
+
+// Identify an alternate config location or only overwrite some items below.
+import { default as DEER } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.9/deer-config.js'
+
+// Identify a UTILS package
+import { default as UTILS } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.9/deer-utils.js'
+
+// Render is probably needed by all items, but can be removed.
+// CDN at https://centerfordigitalhumanities.github.io/deer/releases/
+import { default as renderer, initializeDeerViews } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.9/deer-render.js'
+
+// Record is only needed for saving or updating items.
+// CDN at https://centerfordigitalhumanities.github.io/deer/releases/
+import { default as record, initializeDeerForms } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.9/deer-record.js'
+
+// fire up the element detection as needed
+initializeDeerViews(DEER)
+    //Need to make the form initializer wait on view initializer, these cannot run syncronously.  
+initializeDeerForms(DEER)
